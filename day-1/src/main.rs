d50a00bde718669f2f43e3d97416501d7a2d1f26
michael@zedeler.dk <michael@zedeler.dk>
2023-12-02 02:25:22 +0100

    This should work.

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -4,12 +4,12 @@ use regex::Regex;
 
 const ERROR_MESSAGE: &str = "Some weird error happened. Help!";
 
-fn parse_line(line: &str) {
+fn parse_line(line: &str) -> Vec<i32> {
   let re = Regex::new(r"(?:(\d)|(one|two|three|four|five|six|seven|eight|nine))").unwrap();
-  for captures in re.captures_iter(line) {
+  re.captures_iter(line).map(|captures| {
     let first = captures.get(1);
     let second = captures.get(2);
-    let value = match (first, second) {
+    match (first, second) {
       (Some(digit), _) => digit.as_str().parse().unwrap(),
       (_, Some(number)) => match number.as_str() {
         "zero" => 0,
@@ -25,10 +25,8 @@ fn parse_line(line: &str) {
         _ => panic!("{}", ERROR_MESSAGE),
       },
       (None, None) => panic!("{}", ERROR_MESSAGE),
-    };
-    println!("Value: {}", value);
-  }
-  println!("--");
+    }
+  }).collect()
 }
 
 fn day_1(path: &str) -> i32 {
@@ -36,16 +34,10 @@ fn day_1(path: &str) -> i32 {
   let result = file
     .lines()
     .fold(0, |acc, line| {
-      let re = Regex::new(r"^\D*(?<first>\d).*?(?<last>\d)?\D*$").unwrap();
-      parse_line(&line);
-      acc + match re.captures(&line) {
-        None => 0,
-        Some(captures) => {
-          let first = captures.get(1).map_or("", |m| m.as_str());
-          let last = captures.get(2).map_or(first, |m| m.as_str());
-          let concatenated: String = "".to_owned() + first + last;
-          concatenated.parse().expect(ERROR_MESSAGE)
-        }
+      let v = parse_line(&line);
+      acc + match v.len() {
+        1 => v[0] * 10 + v[0],
+        _ => v[0] * 10 + v[v.len() - 1],
       }
     });
   return result;
@@ -61,7 +53,12 @@ mod tests {
   use super::*;
 
   #[test]
-  fn it_works() {
+  fn it_works_with_test_input() {
     assert_eq!(day_1("test_input"), 142);
+  }
+
+  #[test]
+  fn it_works_with_test_input_2() {
+    assert_eq!(day_1("test_input_2"), 51 + 29 + 53);
   }
 }

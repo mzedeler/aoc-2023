083144f1ede9c9b535c27df8556177d717d90278
michael@zedeler.dk <michael@zedeler.dk>
2023-12-02 02:09:17 +0100

    Trying this out: parser.

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -4,12 +4,40 @@ use regex::Regex;
 
 const ERROR_MESSAGE: &str = "Some weird error happened. Help!";
 
+fn parse_line(line: &str) {
+  let re = Regex::new(r"(?:(\d)|(one|two|three|four|five|six|seven|eight|nine))").unwrap();
+  for captures in re.captures_iter(line) {
+    let first = captures.get(1);
+    let second = captures.get(2);
+    let value = match (first, second) {
+      (Some(digit), _) => digit.as_str().parse().unwrap(),
+      (_, Some(number)) => match number.as_str() {
+        "zero" => 0,
+        "one" => 1,
+        "two" => 2,
+        "three" => 3,
+        "four" => 4,
+        "five" => 5,
+        "six" => 6,
+        "seven" => 7,
+        "eight" => 8,
+        "nine" => 9,
+        _ => panic!("{}", ERROR_MESSAGE),
+      },
+      (None, None) => panic!("{}", ERROR_MESSAGE),
+    };
+    println!("Value: {}", value);
+  }
+  println!("--");
+}
+
 fn day_1(path: &str) -> i32 {
   let file = fs::read_to_string(path).expect("ok");
   let result = file
     .lines()
     .fold(0, |acc, line| {
       let re = Regex::new(r"^\D*(?<first>\d).*?(?<last>\d)?\D*$").unwrap();
+      parse_line(&line);
       acc + match re.captures(&line) {
         None => 0,
         Some(captures) => {

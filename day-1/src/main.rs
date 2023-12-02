db86a014000b23a2ce09fc65f43fba6a24af86ee
michael@zedeler.dk <michael@zedeler.dk>
2023-12-02 01:21:22 +0100

    Part 1.

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,11 +1,26 @@
-use std::{fs};
+use std::fs;
+// use str::parse;
+use regex::Regex;
 
 const ERROR_MESSAGE: &str = "Some weird error happened. Help!";
 
 fn day_1(path: &str) -> i32 {
   let file = fs::read_to_string(path).expect("ok");
-  println!("{}", file);
-  return 1;
+  let result = file
+    .lines()
+    .fold(0, |acc, line| {
+      let re = Regex::new(r"^\D*(?<first>\d).*?(?<last>\d)?\D*$").unwrap();
+      acc + match re.captures(&line) {
+        None => 0,
+        Some(captures) => {
+          let first = captures.get(1).map_or("", |m| m.as_str());
+          let last = captures.get(2).map_or(first, |m| m.as_str());
+          let concatenated: String = "".to_owned() + first + last;
+          concatenated.parse().expect(ERROR_MESSAGE)
+        }
+      }
+    });
+  return result;
 }
 
 fn main() {

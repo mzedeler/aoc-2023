ea5911a4562287d5222231de134120eab1e7e547
michael@zedeler.dk <michael@zedeler.dk>
2023-12-04 23:16:58 +0100

    Finally :P.

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,35 +1,40 @@
 use std::fs;
-// use str::parse;
 use regex::Regex;
 
-const OTHER_REGEX: &str = r"(?<first>\d|zero|one|two|three|four|five|six|seven|eight|nine).*(?<last>\d|zero|one|two|three|four|five|six|seven|eight|nine)?.*?$";
+const FIRST_REGEX: &str = r"(?:(?<digit>\d)|(?<number>zero|one|two|three|four|five|six|seven|eight|nine))";
+const LAST_REGEX: &str = r".*(?:(?<digit>\d)|(?<number>zero|one|two|three|four|five|six|seven|eight|nine))";
 
-const NUMBER_REGEX: &str = r"(?<digit>\d)(?<number>zero|one|two|three|four|five|six|seven|eight|nine)";
-
-fn parse_number(number: &str) -> Option<i32> {
-  match number {
-    "zero" => Some(0),
-    "one" => Some(1),
-    "two" => Some(2),
-    "three" => Some(3),
-    "four" => Some(4),
-    "five" => Some(5),
-    "six" => Some(6),
-    "seven" => Some(7),
-    "eight" => Some(8),
-    "nine" => Some(9),
-    (digit) => digit.parse().ok()
+fn parse_number(regex: Regex, line: &str) -> Option<i32> {
+  match regex.captures(line) {
+    Some(captures) => if let Some(digit) = captures.name("digit") {
+      digit.as_str().parse().ok()
+    } else if let Some(number) = captures.name("number") {
+      match number.as_str() {
+        "zero" => Some(0),
+        "one" => Some(1),
+        "two" => Some(2),
+        "three" => Some(3),
+        "four" => Some(4),
+        "five" => Some(5),
+        "six" => Some(6),
+        "seven" => Some(7),
+        "eight" => Some(8),
+        "nine" => Some(9),
+        _ => None
+      }
+    } else {
+      None
+    },
+    None => None
   }
 }
 
 fn parse_line(line: &str) -> (i32, i32) {
-  let re = Regex::new(OTHER_REGEX).unwrap();
-  println!("Test line: {}", line);
-  let captures = re.captures(line).unwrap();
-  let first_parsed = parse_number(captures.name("first").map_or("", |m| m.as_str()));
-  let last_parsed = parse_number(captures.name("last").map_or("", |m| m.as_str()));
-  println!("T: {:?} {:?}", first_parsed, last_parsed);
-  (first_parsed.unwrap(), last_parsed.unwrap_or(first_parsed.unwrap()))
+  let first_re = Regex::new(FIRST_REGEX).unwrap();
+  let last_re = Regex::new(LAST_REGEX).unwrap();
+  let first_parsed = parse_number(first_re, &line).unwrap();
+  let last_parsed = parse_number(last_re, &line).unwrap_or(first_parsed);
+  (first_parsed, last_parsed)
 }
 
 fn day_1(path: &str) -> i32 {
@@ -38,7 +43,6 @@ fn day_1(path: &str) -> i32 {
     .lines()
     .fold(0, |acc, line| {
       let (first, second) = parse_line(&line);
-      println!("{}, {}", first, second);
       acc + first * 10 + second
     })
 }

bf8f4ebf8a80d59a26c277818a3c6b5849b45832
michael@zedeler.dk <michael@zedeler.dk>
2023-12-04 23:19:10 +0100

    This causes the regexes to be moved - how do I make this work?

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -4,7 +4,7 @@ use regex::Regex;
 const FIRST_REGEX: &str = r"(?:(?<digit>\d)|(?<number>zero|one|two|three|four|five|six|seven|eight|nine))";
 const LAST_REGEX: &str = r".*(?:(?<digit>\d)|(?<number>zero|one|two|three|four|five|six|seven|eight|nine))";
 
-fn parse_number(regex: Regex, line: &str) -> Option<i32> {
+fn parse_number(regex: Box<Regex>, line: &str) -> Option<i32> {
   match regex.captures(line) {
     Some(captures) => if let Some(digit) = captures.name("digit") {
       digit.as_str().parse().ok()
@@ -29,20 +29,20 @@ fn parse_number(regex: Regex, line: &str) -> Option<i32> {
   }
 }
 
-fn parse_line(line: &str) -> (i32, i32) {
-  let first_re = Regex::new(FIRST_REGEX).unwrap();
-  let last_re = Regex::new(LAST_REGEX).unwrap();
+fn parse_line(first_re: Box<Regex>, last_re: Box<Regex>, line: &str) -> (i32, i32) {
   let first_parsed = parse_number(first_re, &line).unwrap();
   let last_parsed = parse_number(last_re, &line).unwrap_or(first_parsed);
   (first_parsed, last_parsed)
 }
 
 fn day_1(path: &str) -> i32 {
+  let first_re = Regex::new(FIRST_REGEX).unwrap();
+  let last_re = Regex::new(LAST_REGEX).unwrap();
   let file = fs::read_to_string(path).expect("ok");
   file
     .lines()
     .fold(0, |acc, line| {
-      let (first, second) = parse_line(&line);
+      let (first, second) = parse_line(Box::new(first_re), Box::new(last_re), &line);
       acc + first * 10 + second
     })
 }

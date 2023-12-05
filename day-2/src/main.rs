3b29420fd186b81ee337d5dec42dd0da40996795
michael@zedeler.dk <michael@zedeler.dk>
2023-12-06 00:52:32 +0100

    First part done.

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -17,18 +17,20 @@ struct Game {
 }
 
 fn parse_line(line: &str) -> Game {
-  println!("{}", line);
   let re = Regex::new(r"Game (\d+):\s+(.*)").expect(UNIVERSAL_ERROR_MESSAGE);
   let (_, [id_str, handfuls_str]) = re.captures(line).unwrap().extract();
   let handfuls: Vec<Handful> = handfuls_str.split(';').map(|handful| {
-    Regex::new(r"(\d+ \w+)")
+    Regex::new(r"(\d+) (\w+)")
       .expect(UNIVERSAL_ERROR_MESSAGE)
-      .captures(handful)
-      .unwrap()
-      .iter()
-      .fold(Handful::default(), |r, m| {
-        println!("{:?}", m.map_or("", |m| m.as_str()));
-        r
+      .captures_iter(handful)
+      .fold(Handful::default(), |result, captures| {
+        let (_, [count_str, color]) = captures.extract();
+        match color {
+          "red" => Handful { red: count_str.parse().expect(UNIVERSAL_ERROR_MESSAGE), ..result },
+          "green" => Handful { green: count_str.parse().expect(UNIVERSAL_ERROR_MESSAGE), ..result },
+          "blue" => Handful { blue: count_str.parse().expect(UNIVERSAL_ERROR_MESSAGE), ..result },
+          other_string => panic!("I have no idea what color this is: {}", other_string),
+        }
       })
   }).collect();
 
@@ -40,17 +42,16 @@ fn parse_line(line: &str) -> Game {
 
 fn day_2(input: &str) -> i32 {
   let file = fs::read_to_string(input).expect(UNIVERSAL_ERROR_MESSAGE);
-  let games: Vec<Game> = file
+  file
     .lines()
     .map(|line| parse_line(line))
-    .collect();
-
-  println!("Game 0: {:?}, {:?}", games[0].id, games[0].handfuls);
-  8
+    .fold(0, |result, game| 
+      result + if game.handfuls.into_iter().all(|handful| handful.red < 13 && handful.green < 14 && handful.blue < 15) { game.id } else { 0 }
+    )
 }
 
 fn main() {
-  day_2("input");
+  println!("{}", day_2("input"));
 }
 
 #[cfg(test)]

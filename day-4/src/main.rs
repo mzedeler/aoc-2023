0d912e87c2890622473910c2adb56103002f2cb4
michael@zedeler.dk <michael@zedeler.dk>
2023-12-11 23:59:00 +0100

    Challenge 2 completed.

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -4,6 +4,8 @@ use std::fs::File;
 use std::collections::HashSet;
 use std::sync::OnceLock;
 use regex::Regex;
+use std::collections::VecDeque;
+use std::cmp::min;
 
 const UNIVERSAL_ERROR_MESSAGE: &str = "Something went wrong. Help!";
 
@@ -20,7 +22,6 @@ fn parse_file(path: &str) -> GameFileIterator {
 
 #[derive(Debug,Default)]
 struct GameCard {
-  card_number: i32,
   winning_numbers: Vec<u32>,
   selected_numbers: Vec<u32>
 }
@@ -41,10 +42,9 @@ fn parse_numbers(input_str: &str) -> Vec<u32> {
 fn parse_line(line: &String) -> GameCard {
   static PART_RE: OnceLock<Regex> = OnceLock::new();
   let part_regex = PART_RE.get_or_init(|| Regex::new(r"Card\s+(\d+)\D+([^|]+)\|(.+)").expect(UNIVERSAL_ERROR_MESSAGE));
-  let (_, [card_number_str, winning_numbers_str, selected_numbers_str]) = part_regex.captures(&line).expect(UNIVERSAL_ERROR_MESSAGE).extract();
+  let (_, [_, winning_numbers_str, selected_numbers_str]) = part_regex.captures(&line).expect(UNIVERSAL_ERROR_MESSAGE).extract();
 
   GameCard {
-    card_number: card_number_str.parse().unwrap(),
     winning_numbers: parse_numbers(winning_numbers_str),
     selected_numbers: parse_numbers(selected_numbers_str),
   }
@@ -84,8 +84,50 @@ fn day_4_1(path: &str) -> u32 {
     .fold(0, |sum, score| sum + score)
 }
 
+trait Part2State {
+  fn record_game_result(&mut self, card_count: u32, score: u32);
+  fn next(&mut self) -> u32;
+}
+
+impl Part2State for VecDeque<u32> {
+  fn record_game_result(&mut self, total_copies: u32, score: u32) {
+    let capacity_limit = min(self.len(), score.try_into().unwrap());
+    for i in 0..capacity_limit {
+      self[i] += total_copies;
+    }
+    for _ in capacity_limit..score.try_into().unwrap() {
+      self.push_back(total_copies);
+    }
+  }
+
+  fn next(&mut self) -> u32 {
+    self.remove(0).unwrap_or(0)
+  }
+}
+
+fn day_4_2(path: &str) -> u32 {
+  let (total_score, _) = parse_file(path)
+    .map(|game_card| {
+      let winning_numbers_hash: HashSet<&u32> = game_card.winning_numbers.iter().collect();
+      let selected_numbers_hash: HashSet<&u32> = game_card.selected_numbers.iter().collect();
+      winning_numbers_hash
+        .intersection(&selected_numbers_hash)
+        .into_iter()
+        .count()
+        .try_into()
+        .unwrap()
+    })
+    .fold((0, VecDeque::<u32>::new()), |(total_score, mut state), score| {
+      let total_copies = state.next() + 1; // + 1 for the original copy
+      state.record_game_result(total_copies, score);
+      (total_score + total_copies, state)
+    });
+    total_score
+}
+
 fn main() {
-    println!("{}", day_4_1("input"));
+  println!("{}", day_4_1("input"));
+  println!("{}", day_4_2("input"));
 }
 
 #[cfg(test)]
@@ -95,5 +137,10 @@ mod tests {
   #[test]
   fn day_4_1_handles_test_input() {
     assert_eq!(day_4_1("test_input"), 13);
+  }
+
+  #[test]
+  fn day_4_2_handles_test_input() {
+    assert_eq!(day_4_2("test_input"), 30);
   }
 }

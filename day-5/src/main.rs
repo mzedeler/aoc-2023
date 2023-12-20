cb82405dc0cdfc61f4b2277ea5d8ffd9d4d19f21
michael@zedeler.dk <michael@zedeler.dk>
2023-12-20 23:35:20 +0100

    Now much simpler.

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -1,85 +1,74 @@
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
-use std::sync::OnceLock;
-use regex::Regex;
 
 const UNIVERSAL_ERROR_MESSAGE: &str = "Something unexpected happened. Help!";
 
-#[derive(Debug,Default)]
-struct Map {
-  dst_start: u32,
-  src_start: u32,
-  length: u32
-}
-
 #[derive(Debug)]
-enum AlmanacSection {
+enum AlmanacItem {
   Seeds(Vec<u32>),
-  Map((String, u32, u32, u32)),
+  Map((u32, u32, u32)),
 }
 
 struct AlmanacIterator {
   reader: BufReader<File>,
-  line: String
+  seeds_emitted: bool
 }
 
 fn parse_file(path: &str) -> AlmanacIterator {
   let file = File::open(path).expect(UNIVERSAL_ERROR_MESSAGE);
   let reader = BufReader::new(file);
-  AlmanacIterator { reader, line: String::new() }
+  AlmanacIterator { reader, seeds_emitted: false }
 }
 
 impl AlmanacIterator {
-  fn parse_seeds(seeds: &str) -> Vec<u32> {
-    static SEEDS_RE: OnceLock<Regex> = OnceLock::new();
-    let part_regex = SEEDS_RE.get_or_init(|| Regex::new(r"(\d+)").expect(UNIVERSAL_ERROR_MESSAGE));
-    part_regex
-      .captures_iter(seeds)
-      .map(|captures| captures.get(1).expect(UNIVERSAL_ERROR_MESSAGE).as_str().parse::<u32>().expect(UNIVERSAL_ERROR_MESSAGE))
-      .collect()
-  }
+  fn next_line(&mut self) -> Option<String> {
+    let mut buffer = String::new();
 
-  fn parse_almanac_section(&mut self) -> Option<AlmanacSection> {
-    static HEADER_RE: OnceLock<Regex> = OnceLock::new();
-    let part_regex = HEADER_RE.get_or_init(|| Regex::new(r"^(?:seeds: (.+)|(\S+) map:|)\s*$").expect(UNIVERSAL_ERROR_MESSAGE));
-    let _ = &self.line.clear();
-    let _ = self.reader.read_line(&mut self.line);
+    let read_length = &self
+      .reader
+      .read_line(&mut buffer)
+      .expect(UNIVERSAL_ERROR_MESSAGE);
 
-    let captures_option = part_regex.captures(&self.line);
-    println!("{:?}: line: '{}'", captures_option, &self.line);
-    let captures = captures_option.expect(UNIVERSAL_ERROR_MESSAGE);
-    match (captures.get(1), captures.get(2)) {
-      (Some(seeds), _) => Some(AlmanacSection::Seeds(AlmanacIterator::parse_seeds(seeds.as_str()))),
-      (_, Some(map_name)) => Some(AlmanacSection::Map((String::from(map_name.as_str()), 1, 1, 1))),
-      (None, None) => // empty line
+    match read_length {
+      0 => None,
+      _ => Some(buffer)
     }
   }
 }
 
 impl Iterator for AlmanacIterator {
-  type Item = AlmanacSection;
+  type Item = AlmanacItem;
   
   fn next(&mut self) -> Option<Self::Item> {
-    let section = self.parse_almanac_section();
-    println!("Section 1: {:?}", section);
-    let section = self.parse_almanac_section();
-    println!("Section 2: {:?}", section);
-    section
-    // self.line.clear();
-    // let result = self.reader.read_line(&mut self.line);
-    // match result {
-    //   Err(_) => None,
-    //   Ok(0) => None,
-    //   Ok(_) => {
-    //     Some(self.parse_almanac_section())
-    //   }
-    // }
+    loop {
+      let buffer = Some(self.next_line())?;
+
+      let numbers: Vec<u32> = buffer.unwrap()
+        .split(|c| c > '9' || c < '0')
+        .filter(|token| token.trim().len() > 0)
+        .map(|token| token.parse::<u32>().unwrap())
+        .collect();
+
+      if numbers.len() > 0 {
+        if self.seeds_emitted {
+          println!("Numbers before emit: {:?}", numbers);
+          break Some(AlmanacItem::Map((numbers[0], numbers[1], numbers[2])))
+        } else {
+          self.seeds_emitted = true;
+          break Some(AlmanacItem::Seeds(numbers))
+        }  
+      }
+    }
   }
 }
 
 fn day_5_1(path: &str) -> u32 {
   let mut almanac_iterator = parse_file(path);
+  println!("-> {:?}", almanac_iterator.next());
+  println!("-> {:?}", almanac_iterator.next());
+  println!("-> {:?}", almanac_iterator.next());
+  println!("-> {:?}", almanac_iterator.next());
   println!("-> {:?}", almanac_iterator.next());
   1
 }
